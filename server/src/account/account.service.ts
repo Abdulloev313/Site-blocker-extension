cd1f8ddf41b1d4cbef40dc741bdc24782e9d4041
Dilshod Abdulloev <Dilshodabdullayev2003@gmail.com>
2024-02-12 13:46:10 +0500

    add await for async functions

--- server/src/account/account.service.ts
+++ server/src/account/account.service.ts
@@ -7,7 +7,7 @@ export class AccountService {
   constructor(private db: DbService) {}
 
   async create(userId: number) {
-    return this.db.account.create({
+    return await this.db.account.create({
       data: {
         ownerId: userId,
         isBlockingEnabled: false,
@@ -16,11 +16,13 @@ export class AccountService {
   }
 
   async getAccount(userId: number) {
-    return this.db.account.findUniqueOrThrow({ where: { ownerId: userId } });
+    return await this.db.account.findUniqueOrThrow({
+      where: { ownerId: userId },
+    });
   }
 
   async patchAccount(userId: number, patch: PatchAccountDto) {
-    return this.db.account.update({
+    return await this.db.account.update({
       where: { ownerId: userId },
       data: { ...patch },
     });
