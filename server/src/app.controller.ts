3629bfe56b8d1ac4833fab14df5aef0c02294fe4
Dilshod Abdulloev <Dilshodabdullayev2003@gmail.com>
2024-02-12 14:04:36 +0500

    delete unnecessary code

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,16 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { DbService } from './db/db.service';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private dbService: DbService,
-  ) {}
-
-  @Get()
-  getHello() {
-    return this.dbService.user.findMany();
-  }
-}
+export class AppController {}
