2973bb1ce4d5b390aef9e901a15f3df9a1628fab
Dilshod Abdulloev <Dilshodabdullayev2003@gmail.com>
2024-03-09 19:58:41 +0500

    delete unnecessary validation

--- server/src/auth/dto/signUp.dto.ts
+++ server/src/auth/dto/signUp.dto.ts
@@ -1,11 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsString,
-  Matches,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SignUpDto {
   @ApiProperty({ example: 'test@gmail.com' })
@@ -16,10 +10,10 @@ export class SignUpDto {
   @ApiProperty({ example: 'Asf3&msj' })
   @IsString()
   @IsNotEmpty()
-  @MinLength(8, { message: 'Пароль должен содержать минимум 8 символов' })
-  @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/, {
-    message:
-      'Пароль должен содержать минимум 1 цифру, 1 заглавную букву и 1 спец. символ',
-  })
+  // @MinLength(8, { message: 'Пароль должен содержать минимум 8 символов' })
+  // @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/, {
+  //   message:
+  //     'Пароль должен содержать минимум 1 цифру, 1 заглавную букву и 1 спец. символ',
+  // })
   password: string;
 }
