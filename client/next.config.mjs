b351dfcae9518407314256ddfe18a71fe3078937
Dilshod Abdulloev <Dilshodabdullayev2003@gmail.com>
2024-03-11 23:11:24 +0500

    implement auth

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8001/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -1,6 +1,14 @@
-import '@/app/globals.css';
-import type {AppProps} from 'next/app';
+import type { AppProps } from "next/app";
+import { AppProvider } from "./app-provider";
+import { Inter } from "next/font/google";
 
-export function App({Component, pageProps}: AppProps) {
-	return <Component {...pageProps} />;
+const inter = Inter({ subsets: ["latin"] });
+export function App({ Component, pageProps }: AppProps) {
+  return (
+    <AppProvider>
+      <div className={inter.className}>
+        <Component {...pageProps} />
+      </div>
+    </AppProvider>
+  );
 }
