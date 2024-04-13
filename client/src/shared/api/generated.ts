9ce9c6b178a5c46de0a5daf10f3c8e0ca7ee4e3e
Dilshod Abdulloev <Dilshodabdullayev2003@gmail.com>
2024-04-13 18:02:36 +0500

    fix bugs

--- client/src/shared/api/generated.ts
+++ client/src/shared/api/generated.ts
@@ -13,7 +13,6 @@ export type BlockListControllerGetListParams = {
 export type AddBlockItemDtoType =
   (typeof AddBlockItemDtoType)[keyof typeof AddBlockItemDtoType];
 
-// eslint-disable-next-line @typescript-eslint/no-redeclare
 export const AddBlockItemDtoType = {
   KeyWord: "KeyWord",
   Website: "Website",
@@ -27,7 +26,6 @@ export interface AddBlockItemDto {
 export type BlockItemDtoType =
   (typeof BlockItemDtoType)[keyof typeof BlockItemDtoType];
 
-// eslint-disable-next-line @typescript-eslint/no-redeclare
 export const BlockItemDtoType = {
   KeyWord: "KeyWord",
   Website: "Website",
